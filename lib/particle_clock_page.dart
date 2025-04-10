24a647b7f6ccdc04ab29e36af98e3118b2f11a33
cscoderr <tomiwaidowuu@gmail.com>
2025-04-10 22:39:01 +0100

    update particle clock page

--- lib/particle_clock_page.dart
+++ lib/particle_clock_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-class ParticleClockPage extends StatelessWidget {
+class ParticleClockPage extends StatefulWidget {
   const ParticleClockPage({super.key});
 
+  @override
+  State<ParticleClockPage> createState() => _ParticleClockPageState();
+}
+
+class _ParticleClockPageState extends State<ParticleClockPage> {
+  double progress = 0.1;
+  double spring = 0.3;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +17,36 @@ class ParticleClockPage extends StatelessWidget {
         title: const Text('Particle Clock Animation'),
         centerTitle: true,
       ),
-      body: Center(child: Text('Particle clock will be here')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Text('12:30:35', style: TextStyle(fontSize: 70)),
+              ),
+            ),
+            Slider.adaptive(
+              value: progress,
+              onChanged: (value) {
+                setState(() {
+                  progress = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            Slider.adaptive(
+              value: spring,
+              onChanged: (value) {
+                setState(() {
+                  spring = value;
+                });
+              },
+            ),
+            SizedBox(height: 50),
+          ],
+        ),
+      ),
     );
   }
 }
